b638c52415a1b0b7c6a8c4526da1e0fe0848918e
yapcheahshen <yapcheahshen@gmail.com>
2015-01-04 01:44:34 +0800

    moved from ksana-document

---
+++ configs.js
@@ -0,0 +1,56 @@
+var tokenizers=require('./tokenizers');
+var normalizeTbl=null;
+var setNormalizeTable=function(tbl,obj) {
+	if (!obj) {
+		obj={};
+		for (var i=0;i<tbl.length;i++) {
+			var arr=tbl[i].split("=");
+			obj[arr[0]]=arr[1];
+		}
+	}
+	normalizeTbl=obj;
+	return obj;
+}
+var normalize1=function(token) {
+	if (!token) return "";
+	token=token.replace(/[ \n\.,，。！．「」：；、]/g,'').trim();
+	if (!normalizeTbl) return token;
+	if (token.length==1) {
+		return normalizeTbl[token] || token;
+	} else {
+		for (var i=0;i<token.length;i++) {
+			token[i]=normalizeTbl[token[i]] || token[i];
+		}
+		return token;
+	}
+}
+var isSkip1=function(token) {
+	var t=token.trim();
+	return (t=="" || t=="　" || t=="※" || t=="\n");
+}
+var normalize_tibetan=function(token) {
+	return token.replace(/[།་ ]/g,'').trim();
+}
+
+var isSkip_tibetan=function(token) {
+	var t=token.trim();
+	return (t=="" || t=="　" ||  t=="\n");	
+}
+var simple1={
+	func:{
+		tokenize:tokenizers.simple
+		,setNormalizeTable:setNormalizeTable
+		,normalize: normalize1
+		,isSkip:	isSkip1
+	}
+	
+}
+var tibetan1={
+	func:{
+		tokenize:tokenizers.tibetan
+		,setNormalizeTable:setNormalizeTable
+		,normalize:normalize_tibetan
+		,isSkip:isSkip_tibetan
+	}
+}
+module.exports={"simple1":simple1,"tibetan1":tibetan1}
---
+++ index.js
@@ -0,0 +1,37 @@
+/* 
+  custom func for building and searching ydb
+
+  keep all version
+  
+  getAPI(version); //return hash of functions , if ver is omit , return lastest
+	
+  postings2Tree      // if version is not supply, get lastest
+  tokenize(text,api) // convert a string into tokens(depends on other api)
+  normalizeToken     // stemming and etc
+  isSpaceChar        // not a searchable token
+  isSkipChar         // 0 vpos
+
+  for client and server side
+  
+*/
+var configs=require("./configs");
+var config_simple="simple1";
+var optimize=function(json,config) {
+	config=config||config_simple;
+	return json;
+}
+
+var getAPI=function(config) {
+	config=config||config_simple;
+	var func=configs[config].func;
+	func.optimize=optimize;
+	if (config=="simple1") {
+		//add common custom function here
+	} else if (config=="tibetan1") {
+
+	} else throw "config "+config +"not supported";
+
+	return func;
+}
+
+module.exports={getAPI:getAPI};
